f0e23acf053ff2f4be3e8b542fac706ed3c19e9c
Yury <yury.fomichov@gmail.com>
2021-11-01 19:04:23 +0100

    update

--- src/middlewares/require-auth.ts
+++ src/middlewares/require-auth.ts
@@ -1,7 +1,11 @@
-import { NextFunction, Request, Response } from 'express';
-import { NotAuthrizedError } from '../errors/not-authorized-error';
+import { NextFunction, Request, Response } from "express";
+import { NotAuthrizedError } from "../errors/not-authorized-error";
 
-export const requireAuthMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const requireAuthMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (!req.currentUser) {
     throw new NotAuthrizedError();
   }
